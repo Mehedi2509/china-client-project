0666d701f0497bfc6a04fc13211eac5a55cd9862
MD MEHEDI HASAN PARVEZ <mehedi25092@gmail.com>
2025-07-26 22:44:28 +0600

    Upload three page

---
+++ new.php
@@ -0,0 +1,96 @@
+<section class="py70 ql-faq lightbox" style="" >
+    <div class="container mx-auto px-4 lg:px-0 lightbox-content">
+        <div class="flex flex-col ql-1024-flex-row">
+            <div class="lg:w-1/2 lg:pr-20   rounded-lg  mb-8 lg:mb-0 text-white form-container">
+                <h2 class="ql_section_title mb12">
+                    Promise Reply in 4 hours !
+                </h2>
+                <?php echo do_shortcode('[contact-form-7 id="dba2b5a" title="Contact us"]'); ?>
+            </div>
+
+            <div
+                class="lg:w-1/2  lg:pl-20 rounded-lg  ml-0 faq-container">
+                <h2 class="ql_section_title text-white mb40">
+                    Frequently Ask Questions
+                </h2>
+                <div class="space-y-4">
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(1)">
+                        <span class="faq_no">1)</span> What safety rules do your harnesses follow?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-1"
+                            class="hidden pt-10 text-white">
+                            Our control panel wire harnesses follow big safety rules like UL 508A, RoHS, CE, and ISO 9001. These rules help make sure the control panel wire harness is safe and works well.
+                        </div>
+                    </button>
+
+
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(2)">
+                        <span class="faq_no">2)</span> How do you make sure the harness fits my panel?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-2"
+                            class="hidden pt-10 text-white">
+                            We use your Wiring Harness for Electrical Panels drawing and design plans to build a control panel wire harness that fits just right. We check everything so the final harness matches your system.
+						</div>
+                    </button>
+
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(3)">
+                        <span class="faq_no">3)</span> Can I get a sample to try before I order more?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-3"
+                            class="hidden pt-10 text-white">
+                            Yes. We can send you a small test harness. You can check it and ensure it works before ordering a bigger amount.
+                    </button>
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(4)">
+                        <span class="faq_no">4)</span> How long does it take to get my harness?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-4"
+                            class="hidden pt-10 text-white">
+                            Most orders take a few weeks. But if you need it sooner, we can work faster. We also have rush service for urgent jobs.
+                    </button>
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(5)">
+                        <span class="faq_no">5)</span> Do you give drawings or diagrams with the harness?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-5"
+                            class="hidden pt-10 text-white">
+                            Yes. We provide simple 2D or 3D diagrams. These show you how to install the control panel wire harness and where each wire goes.
+                    </button>
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(6)">
+                        <span class="faq_no">6)</span> Can you use the same connectors I already use?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-6"
+                            class="hidden pt-10 text-white">
+                            Yes. We can use the same type or one that matches closely. You don’t need to change your current setup.
+                    </button>
+                    <button
+                        class="w-full text-left px-4 py-2 rounded-lg border text-white border-white font-light"
+                        onclick="toggleFAQ(7)">
+                        <span class="faq_no">7)</span> Do you add labels or tags to the wires?
+                        <span class="ml-2 text-white float-right text-3xl toggle-symbol">+</span>
+                        <div
+                            id="faq-content-7"
+                            class="hidden pt-10 text-white">
+                            Yes. We can add names, barcodes, or tags to each wire or group. This helps you install and track them easily.
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
